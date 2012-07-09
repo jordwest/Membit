969d1236b9616522cc05fa6deee306a21384e19a
Jordan West <jordwest@gmail.com>
2012-07-09 13:26:29 +1000

    initial commit - empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Membit::Application.config.secret_token = '47959f6763b0da17d72f48314e789c66d730b9a5e0a9a0f0593faacbe603d7c9c003f0e7b7201cbe729d6668e02a61f1bd82a16dd89f63bd871018730d0ac398'
